b03c14dd85f37641449af8c0019f5b8ba3d64580
emmanueletti <78582921+emmanueletti@users.noreply.github.com>
2021-06-10 15:39:24 -0700

    created method for dynamically opening calculators when card is clicked

---
+++ script.js
@@ -0,0 +1,20 @@
+import { calc0 } from './components/calc-components.js';
+
+const calculatorCards = document.querySelectorAll('.card');
+
+const mainBodyContainer = document.querySelector('.main-container');
+
+// add click event listener to each card
+calculatorCards.forEach((card, index) => {
+	card.addEventListener('click', () => {
+		// hide all cards
+		calculatorCards.forEach((card) => {
+			card.classList.toggle('hidden');
+		});
+
+		// add the correct card to the site by dynamically generating the name of the correct calc()import based on the card nodeList index  - the title of the imported calculator functions have been named to match the index of the cards
+		let calculatorFunction = `calc${index}()`;
+
+		mainBodyContainer.innerHTML = eval(calculatorFunction);
+	});
+});
