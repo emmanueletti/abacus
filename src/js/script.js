1a60990d12bf1308f6a44f39fe996234dea61ac1
emmanueletti <78582921+emmanueletti@users.noreply.github.com>
2021-06-17 15:17:29 -0700

    finished investment fees calculator functionality

--- src/js/script.js
+++ src/js/script.js
@@ -19,6 +19,84 @@ calculatorCards.forEach((card, index) => {
 	});
 });
 
+// event listener - every change to input value will be stored in object
+const investmentFeesData = {
+	currentAmount: 5000,
+	annualContribution: 1000,
+	currentAge: 18,
+	retirementAge: 65,
+	annualReturn: 7,
+	mer: 2.23,
+};
+
+// can this copy and paste job be simplified?
+const currentAmount = document.querySelector('#current-amount');
+const annualContribution = document.querySelector('#contribution-amount');
+const currentAge = document.querySelector('#current-age');
+const retirementAge = document.querySelector('#end-age');
+const annualReturn = document.querySelector('#annual-return');
+const mer = document.querySelector('#mer');
+
+currentAmount.addEventListener('input', (e) => {
+	investmentFeesData.currentAmount = Number(e.target.value);
+});
+annualContribution.addEventListener('input', (e) => {
+	investmentFeesData.annualContribution = Number(e.target.value);
+});
+currentAge.addEventListener('input', (e) => {
+	investmentFeesData.currentAge = Number(e.target.value);
+});
+retirementAge.addEventListener('input', (e) => {
+	investmentFeesData.retirementAge = Number(e.target.value);
+});
+annualReturn.addEventListener('input', (e) => {
+	investmentFeesData.annualReturn = parseFloat(e.target.value);
+});
+mer.addEventListener('input', (e) => {
+	investmentFeesData.mer = parseFloat(e.target.value);
+});
+
 // get calculator results from API
 
-// investment returns calculation
+const investmentFeesSubmit = document.querySelector('#calulate-investment-fees');
+
+// post request to API
+
+const totalWithoutFees = document.querySelector('#totalWithoutFees');
+const amountOfFees = document.querySelector('#amountOfFees');
+const percentToKeep = document.querySelector('#percentToKeep');
+
+const processNumber = (number) => {
+	let processed = Math.round(number).toLocaleString();
+	return processed;
+};
+
+investmentFeesSubmit.addEventListener('click', () => {
+	fetch('http://localhost:5000/api/investment-returns', {
+		method: 'POST',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(investmentFeesData),
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			// investment api returns calculation
+			totalWithoutFees.innerHTML = `$${processNumber(data.totalWithoutFees)}`;
+			amountOfFees.innerHTML = `$${processNumber(data.totalWithoutFees - data.totalWithFees)}`;
+			percentToKeep.innerHTML = `${(data.totalWithFees / data.totalWithoutFees).toFixed(2) * 100}%`;
+
+			let percent = (data.totalWithFees / data.totalWithoutFees).toFixed(2) * 100;
+
+			if (percent < 70) {
+				console.log('TOO MUCH');
+				amountOfFees.style.color = 'red';
+				percentToKeep.style.color = 'red';
+			} else {
+				amountOfFees.style.color = 'black';
+				percentToKeep.style.color = 'green';
+			}
+			console.log(percent);
+		});
+});
