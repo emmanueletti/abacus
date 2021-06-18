5523b8ac2cce4f8766674c82fa3f254291b20db8
emmanueletti <78582921+emmanueletti@users.noreply.github.com>
2021-06-17 18:01:21 -0700

    project is now fully functional, now in need of refactoring and code clean up

--- src/js/components/calculators.js
+++ src/js/components/calculators.js
@@ -1,59 +1,86 @@
 const calc0 = () => {
 	return `<div class="container calc-0">
-	<div class="calc header">
-		<h1 class="calc-0__title">How Much Are You Truely Paying in Fees?</h1>
-		<p class="calc-0__body">
-			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt doloribus, quo blanditiis ut sed magni eligendi nemo inventore! Dolorum Lorem
-			ipsum dolor, sit amet consectetur adipisicing elit. Deserunt doloribus, quo blanditiis ut sed magni eligendi nemo inventore!
-		</p>
-	</div>
+				<div class="calc header">
+					<h1 class="calc-0__title">How Much Are You Truely Paying in Fees?</h1>
+					<p class="calc-0__body">
+						One of the biggest determinants of how much you end with up when you retire is the fees you pay during your investing years. Beyond making
+						regular contributions to tax advantaged accounts like your TFSA and RRSP, the second best thing you can do for your future self is to keep
+						an eye on the management fees charged by your advisor, bank, brokerage, and/or investment fund.
+					</p>
+				</div>
 
-	<div class="calc result">
-		<div class="fees-result">
-			<h1 class="result__title">200k</h1>
-			<p class="result__text">TOTAL AT RETIRMENT</p>
-		</div>
-		<div class="fees-result left-border right-border">
-			<h1 class="result__title">100k</h1>
-			<p class="result__text">LOST IN FEES</p>
-		</div>
-		<div class="fees-result">
-			<h1 class="result__title">50%</h1>
-			<p class="result__text">LEFT TO KEEP</p>
-		</div>
-	</div>
+				<div class="calc result">
+					<div class="fees-result">
+						<h1 class="result__title" id="totalWithoutFees">-</h1>
+						<p class="result__text">TOTAL AT RETIRMENT</p>
+					</div>
+					<div class="fees-result left-border right-border">
+						<h1 class="result__title" id="amountOfFees">-</h1>
+						<p class="result__text">LOST IN FEES</p>
+					</div>
+					<div class="fees-result">
+						<h1 class="result__title" id="percentToKeep">-</h1>
+						<p class="result__text">LEFT TO KEEP</p>
+					</div>
+				</div>
 
-	<div class="calc row">
-		<div class="calc col">
-			<h3>Current Situation</h3>
-			<label for="current-amount">How much do you have invested?</label>
-			<input class="" type="number" name="current-amount" id="current-amount" placeholder="5000" />
-			<label for="contribution-amount">How much do you contribute yearly?</label>
-			<input class="" type="number" name="contribution-amount" id="contribution-amount" placeholder="1000" />
-			<label for="current-age">How old are you?</label>
-			<input class="" type="number" id="current-age" name="current-age" placeholder="18" min="18" max="100" />
-			<p class="input-tip">(Input number between 18 and 100)</p>
-		</div>
-		<div class="calc col">
-			<h3>Retirement</h3>
-			<label for="end-age">What age do you hope to retire?</label>
-			<input class="" type="number" id="end-age" name="end-age" placeholder="65" min="18" max="100" />
-			<p class="input-tip">(Input number between 18 and 100)</p>
-			<h3>Investment Detail</h3>
-			<label for="mer">What is your <span>Management Expense Ratio (M.E.R)?</span></label>
-			<input class="" type="number" id="mer" name="mer" min="0" max="5" step="0.1" placeholder="2.23" /><span> %</span>
-			<p class="input-tip">(Input number between 0 and 5)</p>
-		</div>
-	</div>
-	<h3>Assumptions</h3>
-	<ul>
-		<li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, fuga.</li>
-		<li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, est?</li>
-		<li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, dignissimos.</li>
-		<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, deserunt!</li>
-		<li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, ab.</li>
-	</ul>
-</div>`;
+				<div class="calc row">
+					<div class="calc col">
+						<h3>Current Situation</h3>
+						<label for="current-amount">How much do you have invested?</label>
+						<input class="" type="number" name="current-amount" id="current-amount" value="10000" />
+						<label for="contribution-amount">How much do you contribute yearly?</label>
+						<input class="" type="number" name="contribution-amount" id="contribution-amount" value="6000" />
+						<label for="current-age">How old are you?</label>
+						<input class="" type="number" id="current-age" name="current-age" value="32" min="18" max="100" />
+						<p class="input-tip">(Input number between 18 and 100)</p>
+					</div>
+					<div class="calc col">
+						<h3>Retirement</h3>
+						<label for="end-age">What age do you hope to retire?</label>
+						<input class="" type="number" id="end-age" name="end-age" value="65" min="18" max="100" />
+						<p class="input-tip">(Input number between 18 and 100)</p>
+						<h3>Investment Detail</h3>
+						<label for="annual-return">What annual return do you anticipate? </label>
+						<input class="" type="number" id="annual-return" name="annual-return" min="0" max="10" step="0.1" value="6" /><span> %</span>
+						<p class="input-tip">(Input number between 0 and 10)</p>
+
+						<label for="mer">What is your investments annual management fees? <br />(aka <span>Management Expense Ratio (M.E.R))</span></label>
+						<input class="input-enlarged" type="number" id="mer" name="mer" min="0" max="10" step="0.1" value="2.23" /><span> %</span>
+						<p class="input-tip">(Input number between 0 and 10)</p>
+					</div>
+				</div>
+				<div class="calculator-btn" id="calulate-investment-fees">Calculate</div>
+				<h3>Assumptions</h3>
+				<ol>
+					<li>Annual contributions are added at the end of year as opposed to the beginning of the year</li>
+					<li>The average Canadian pays 2.23% a year in investment fees</li>
+					<li>The average Canadian starts investing at 32</li>
+					<li>The earliest retirement age for Canadians is 65</li>
+				</ol>
+				<h3>Resources</h3>
+				<p class="">
+					None of these links are affiliate. Clicking them or signing up does not provide me with any commisions or rewards. Nothing to sell here.
+				</p>
+				<ol>
+					<li>
+						<a href="https://www.wealthsimple.com/en-ca/product/invest">Wealthsimple</a> is one of Canada's most cost effective robo-advisors. Great
+						For those who want to put their investments on autopilot.
+					</li>
+					<li>
+						<a href="https://www.questrade.com/home">Questrade</a> is one of Canada's most cost effective discount brokerage. Great for those who feel
+						confident enough to learn and manage a DIY portfolio.
+					</li>
+					<li>
+						<a href="https://passiv.com/">Passiv</a> is an incredible tool for Canadian DIY investors who don't want to deal with spreadsheets and
+						portfolio rebalancing calculations.
+					</li>
+					<li>
+						<a href="https://canadiancouchpotato.com/getting-started/">Canadian Couch Potato</a> is a great starting place for Canadians wanting to
+						learn about DIY passive index investing (THE best investment strategy for the average person).
+					</li>
+				</ol>
+			</div>`;
 };
 
 export { calc0 };
--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,104 @@
 import { calc0 } from './components/calculators.js';
 
 const calculatorCards = document.querySelectorAll('.card');
-
-const mainBodyContainer = document.querySelector('.main-container');
+const calcContainer = document.getElementById('calc-container');
+const backButton = document.getElementById('back-btn');
 
 // add click event listener to each card
 calculatorCards.forEach((card, index) => {
 	card.addEventListener('click', () => {
+		// reveal back button
+		backButton.classList.toggle('hidden');
+		backButton.addEventListener('click', (e) => {
+			backButton.classList.add('hidden');
+			calcContainer.innerHTML = '';
+			calculatorCards.forEach((card) => {
+				card.classList.remove('hidden');
+			});
+		});
+
 		// hide all cards
 		calculatorCards.forEach((card) => {
 			card.classList.toggle('hidden');
 		});
 
 		// add the correct card to the site by dynamically generating the name of the correct calc()import based on the card nodeList index  - the title of the imported calculator functions have been named to match the index of the cards
-		let calculatorFunction = `calc${index}()`;
+		let calculatorContent = `calc${index}()`;
 
-		mainBodyContainer.innerHTML = eval(calculatorFunction);
+		// add in the correct calculators HTML
+		calcContainer.innerHTML = eval(calculatorContent);
+
+		// create the calculator functionality
+		const currentAmount = document.querySelector('#current-amount');
+		const annualContribution = document.querySelector('#contribution-amount');
+		const currentAge = document.querySelector('#current-age');
+		const retirementAge = document.querySelector('#end-age');
+		const annualReturn = document.querySelector('#annual-return');
+		const mer = document.querySelector('#mer');
+
+		currentAmount.addEventListener('input', (e) => {
+			investmentFeesData.currentAmount = Number(e.target.value);
+		});
+		annualContribution.addEventListener('input', (e) => {
+			investmentFeesData.annualContribution = Number(e.target.value);
+		});
+		currentAge.addEventListener('input', (e) => {
+			investmentFeesData.currentAge = Number(e.target.value);
+		});
+		retirementAge.addEventListener('input', (e) => {
+			investmentFeesData.retirementAge = Number(e.target.value);
+		});
+		annualReturn.addEventListener('input', (e) => {
+			investmentFeesData.annualReturn = parseFloat(e.target.value);
+		});
+		mer.addEventListener('input', (e) => {
+			investmentFeesData.mer = parseFloat(e.target.value);
+		});
+
+		// get calculator results from API
+		const investmentFeesSubmit = document.querySelector('#calulate-investment-fees');
+
+		// post request to API
+		const totalWithoutFees = document.querySelector('#totalWithoutFees');
+		const amountOfFees = document.querySelector('#amountOfFees');
+		const percentToKeep = document.querySelector('#percentToKeep');
+
+		const processNumber = (number) => {
+			let processed = Math.round(number).toLocaleString();
+			return processed;
+		};
+
+		investmentFeesSubmit.addEventListener('click', () => {
+			fetch('http://localhost:5000/api/investment-returns', {
+				method: 'POST',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(investmentFeesData),
+			})
+				.then((response) => response.json())
+				.then((data) => {
+					// investment api returns calculation
+					totalWithoutFees.innerHTML = `$${processNumber(data.totalWithoutFees)}`;
+					amountOfFees.innerHTML = `$${processNumber(data.totalWithoutFees - data.totalWithFees)}`;
+					percentToKeep.innerHTML = `${(data.totalWithFees / data.totalWithoutFees).toFixed(2) * 100}%`;
+
+					let percent = (data.totalWithFees / data.totalWithoutFees).toFixed(2) * 100;
+
+					if (percent < 70) {
+						amountOfFees.style.color = 'red';
+						percentToKeep.style.color = 'red';
+					} else {
+						amountOfFees.style.color = 'black';
+						percentToKeep.style.color = 'green';
+					}
+				});
+		});
 	});
 });
+
+// go back button
 
 // event listener - every change to input value will be stored in object
 const investmentFeesData = {
@@ -30,73 +111,3 @@ const investmentFeesData = {
 };
 
 // can this copy and paste job be simplified?
-const currentAmount = document.querySelector('#current-amount');
-const annualContribution = document.querySelector('#contribution-amount');
-const currentAge = document.querySelector('#current-age');
-const retirementAge = document.querySelector('#end-age');
-const annualReturn = document.querySelector('#annual-return');
-const mer = document.querySelector('#mer');
-
-currentAmount.addEventListener('input', (e) => {
-	investmentFeesData.currentAmount = Number(e.target.value);
-});
-annualContribution.addEventListener('input', (e) => {
-	investmentFeesData.annualContribution = Number(e.target.value);
-});
-currentAge.addEventListener('input', (e) => {
-	investmentFeesData.currentAge = Number(e.target.value);
-});
-retirementAge.addEventListener('input', (e) => {
-	investmentFeesData.retirementAge = Number(e.target.value);
-});
-annualReturn.addEventListener('input', (e) => {
-	investmentFeesData.annualReturn = parseFloat(e.target.value);
-});
-mer.addEventListener('input', (e) => {
-	investmentFeesData.mer = parseFloat(e.target.value);
-});
-
-// get calculator results from API
-
-const investmentFeesSubmit = document.querySelector('#calulate-investment-fees');
-
-// post request to API
-
-const totalWithoutFees = document.querySelector('#totalWithoutFees');
-const amountOfFees = document.querySelector('#amountOfFees');
-const percentToKeep = document.querySelector('#percentToKeep');
-
-const processNumber = (number) => {
-	let processed = Math.round(number).toLocaleString();
-	return processed;
-};
-
-investmentFeesSubmit.addEventListener('click', () => {
-	fetch('http://localhost:5000/api/investment-returns', {
-		method: 'POST',
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify(investmentFeesData),
-	})
-		.then((response) => response.json())
-		.then((data) => {
-			// investment api returns calculation
-			totalWithoutFees.innerHTML = `$${processNumber(data.totalWithoutFees)}`;
-			amountOfFees.innerHTML = `$${processNumber(data.totalWithoutFees - data.totalWithFees)}`;
-			percentToKeep.innerHTML = `${(data.totalWithFees / data.totalWithoutFees).toFixed(2) * 100}%`;
-
-			let percent = (data.totalWithFees / data.totalWithoutFees).toFixed(2) * 100;
-
-			if (percent < 70) {
-				console.log('TOO MUCH');
-				amountOfFees.style.color = 'red';
-				percentToKeep.style.color = 'red';
-			} else {
-				amountOfFees.style.color = 'black';
-				percentToKeep.style.color = 'green';
-			}
-			console.log(percent);
-		});
-});
